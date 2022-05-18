38c1745ec2d4042910d4c7b410bab26bfea9e822
Bhagawaan <bhagabanghadai043@gmail.com>
2022-05-18 17:55:58 +0530

    url-shortner

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -1,53 +1,98 @@
 const urlModel = require("../models/urlModel")
 const shortid = require('shortid');
+const redis = require("redis");
+const { promisify } = require("util");
+
+
+/*******************************************************************Redis Connection************************************************************/
+
+const redisClient = redis.createClient(
+    14961,
+    "redis-14961.c264.ap-south-1-1.ec2.cloud.redislabs.com", { no_ready_check: true }
+);
+redisClient.auth("pzpNsbz1lxgvWQSwUalsr2wt4YvjYcy5", function (err) {
+    if (err) throw err;
+});
+
+redisClient.on("connect", async function () {
+    console.log("Redis is Connected!!!");
+});
+
+const SET_ASYNC = promisify(redisClient.SET).bind(redisClient);
+const GET_ASYNC = promisify(redisClient.GET).bind(redisClient);
+
+/*************************************************************************************************************************************************/
 
 const isValid = function (value) {
     if (typeof value === 'undefined' || value === null) return false
     if (typeof value === 'string' && value.trim().length === 0) return false
     return true;
 }
+/*****************************************************************Creating Short URL*************************************************************/
 
-/***********************************************************************************************************************/
 const shortUrl = async function (req, res) {
-    const data = req.body
+    try {
 
-    if (!Object.keys(data).length) {
-        return res.status(400).send({ status: false, message: "Invalid request parameters. Please provide URL details" });
+        const data = req.body
+
+        const cachedlongUrl = await GET_ASYNC(`${req.body.longUrl}`); 
+        if (cachedlongUrl) return res.status(200).send(cachedlongUrl);       /*Checking Data From Cache */
+
+        if (!Object.keys(data).length)return res.status(400).send({ status: false, message: "Please provide URL details" }); 
+        
+        if (!isValid(data.longUrl))return res.status(400).send({ status: false, message: "Please provide Long URL." });
+
+        if (!/(http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-/]))?/.test(data.longUrl)){ /*URL validation*/
+            return res.status(400).send({ status: false, message: "Please provide a valid Long URL." })}
+
+        const checkLongUrl = await urlModel.findOne({ longUrl: data.longUrl }).select({ createdAt: 0, updatedAt: 0, __v: 0, _id: 0 }); /*Checking Data From urlModel */
+
+        if (checkLongUrl) {
+        return res.status(200).send({ status: true, message: `Short URL already generated for this longURL.`, data: checkLongUrl });}
+
+        const shortCode = shortid.generate()
+        const baseUrl = "http://localhost:3000";
+        const shortUrl = baseUrl + "/" + shortCode;  /*concatenating base baseURL & URLcode*/
+
+        const ShortenUrl = await urlModel.create({ longUrl: data.longUrl, shortUrl: shortUrl, urlCode: shortCode, });
+        
+        await SET_ASYNC(`${data.longUrl}`, JSON.stringify(ShortenUrl));
+
+        return res.status(201).send({ status: true, message: `Successfully Shorten the URL`, data: ShortenUrl, });
+
+    } catch (err) {
+        res.status(500).send({ status: false, error: err.message });
     }
-    if (!isValid(data.longUrl)) {
-        return res.status(400).send({ status: false, message: "Please provide Long URL." });
-    }
-    const longUrlLowerCase = data.longUrl
-    //checking long URL from DB
-    const checkLongUrl = await urlModel.findOne({ longUrl: longUrlLowerCase }).select({ createdAt: 0, updatedAt: 0, __v: 0 ,_id:0});
-    if (checkLongUrl) {
-        return res.status(200).send({ status: true, message: `Short URL already generated for this longURL.`, data: checkLongUrl });
-    }
-
-    const shortCode = shortid.generate()
-    const baseUrl = "http://localhost:3000";
-
-    const shortUrl = baseUrl + "/" + shortCode; //concatenating base URL & URL code
-
-    const ShortenUrl = await urlModel.create({ longUrl: longUrlLowerCase, shortUrl: shortUrl, urlCode: shortCode, });
-
-    return res.status(201).send({ status: true, message: `Successfully Shorten the URL`, data: ShortenUrl, });
 }
-/***********************************************************************************************************************/
+
+
+/***************************************************************Redirect to Original Url**********************************************************/
+
 const originalUrl = async function (req, res) {
 
-    let shortId = req.params.urlCode;
+    try {
 
-    if(!shortid.isValid(shortId))return res.status(400).send({ status: false, message: "Please provide Correct urlCode." });
-    
-    const originalUrlData = await urlModel.findOne({urlCode: shortId});
-    
-    if (originalUrlData) {
-        return res.redirect(307, originalUrlData.longUrl);
-      } else {
-        return res.status(404).send({ status: false, msg: "No URL Found" });
-      }
-    
+        let cachedShortId = await GET_ASYNC(`${req.params.urlCode}`);
+        let parsedShortId=JSON.parse(cachedShortId)
+        if (parsedShortId) return res.status(307).redirect(parsedShortId.longUrl); /*Checking Data From Cache */
+        
+        if (!shortid.isValid(`${req.params.urlCode}`)) return res.status(400).send({ status: false, message: "Please provide Correct urlCode." }); /*Checking Data From Cache */
+        
+        const originalUrlData = await urlModel.findOne({ urlCode: req.params.urlCode });
+        
+        await SET_ASYNC(`${req.params.urlCode}`, JSON.stringify(originalUrlData));
+
+        if (originalUrlData) {
+            return res.status(307).redirect(originalUrlData.longUrl);
+        } else {
+            return res.status(404).send({ status: false, msg: "No URL Found" });
+        }
+
+    } catch (err) {
+        res.status(500).send({ status: false, error: err.message });
+    }
 }
+
+
 
 module.exports = { shortUrl, originalUrl }
